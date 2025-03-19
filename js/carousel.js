1df2167c73ea3780e9f74814e61f0281317dc8d6
Acauã Rangel <71088524+Acaua-Rangel@users.noreply.github.com>
2025-03-19 11:40:04 -0300

    carrossel

--- js/carousel.js
+++ js/carousel.js
@@ -1,32 +1,80 @@
-
-
-//carousel
-
-//Array storage class
-let carouselArr = [];
-
-
-//class Carousel
 class Carousel {
+    constructor(images, interval = 2000) {
+        this.images = images;
+        this.index = 0;
+        this.intervalTime = interval;
+        this.timer = null;
 
-    
-      
-    static Start(arr){
-        if(arr){
+        // Elementos do carrossel
+        this.imageElement = document.getElementById("carouselImage");
+        this.titleElement = document.getElementById("carouselTitle");
+        this.linkElement = document.getElementById("carouselLink");
 
-            if(arr.length > 0){
-                Carousel._sequence = 0;
-                Carousel._size = arr.length;
-                Carousel.Next(); //start
-                Carousel._interval = setInterval(function(){ Carousel.Next(); },5000);
-            }
-            
-        } else {
-            throw "Method Start need a Array Variable.";
-        }
+        // Botões
+        this.prevBtn = document.getElementById("prevBtn");
+        this.nextBtn = document.getElementById("nextBtn");
+
+        // Eventos
+        this.prevBtn.addEventListener("click", () => this.back());
+        this.nextBtn.addEventListener("click", () => this.next());
+
+        // Inicia o carrossel
+        this.updateCarousel();
+        this.startAutoSlide();
     }
 
-    static Next(){
-        
+    updateCarousel() {
+        const currentItem = this.images[this.index];
+
+        // Atualiza imagem, título e link
+        this.imageElement.src = currentItem.image;
+        this.imageElement.alt = currentItem.title;
+        this.titleElement.textContent = currentItem.title;
+        this.linkElement.href = currentItem.url;
     }
-};
+
+    next() {
+        this.index = (this.index + 1) % this.images.length;
+        this.updateCarousel();
+        this.resetTimer();
+    }
+
+    back() {
+        this.index = (this.index - 1 + this.images.length) % this.images.length;
+        this.updateCarousel();
+        this.resetTimer();
+    }
+
+    startAutoSlide() {
+        this.timer = setInterval(() => this.next(), this.intervalTime);
+    }
+
+    resetTimer() {
+        clearInterval(this.timer);
+        this.startAutoSlide();
+    }
+}
+
+// Dados do carrossel
+const images = [
+    {
+        image: "img/imagem_1.jpg",
+        title: "Esta é a nova Ranger Ford 2022",
+        url: "lancamento.html"
+    },
+    {
+        image: "img/imagem_2.jpg",
+        title: "Ford a nossa história",
+        url: "#"
+    },
+    {
+        image: "img/imagem_3.jpg",
+        title: "Nova Ford Bronco Sport 2022",
+        url: "lancamento.html"
+    }
+];
+
+// Inicializa o carrossel quando a página carregar
+document.addEventListener("DOMContentLoaded", () => {
+    new Carousel(images);
+});
